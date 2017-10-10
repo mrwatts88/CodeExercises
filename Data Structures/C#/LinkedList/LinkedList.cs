1b5d6908ec15fe5c7ad1a2868b4e42cb2443e968
Matthew Watts <Matthew Watts>
2017-10-10 12:44:22 -0500

    C sharp linked list

---
+++ Data Structures/C#/LinkedList/LinkedList.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace myNamespace
+{
+    public class Node
+    {
+        public int value;
+        public Node next = null;
+
+        public Node(int value)
+        {
+            this.value = value;
+        }
+    }
+
+    class LinkedList
+    {
+
+        Node head = null;
+
+        public LinkedList(int[] input)
+        {
+            foreach (int x in input)
+                this.addToEnd(x);
+        }
+
+        public void addToStart(int val)
+        {
+            Node n = new Node(val);
+            n.next = this.head;
+            this.head = n;
+        }
+
+        public void addToEnd(int val)
+        {
+            Node n = this.head;
+            if(n == null)
+            {
+                this.head = new Node(val);
+            }else
+            {
+                while (n.next != null)
+                {
+                    n = n.next;
+                }
+
+                n.next = new Node(val);
+            }
+        }
+
+        public int[] toArray()
+        {
+            Node n = this.head;
+            if (n == null)
+            {
+                return new int[] { };
+            }
+            else
+            {
+                int[] arr = new int[100];
+                int index = 0;
+
+                while (n.next != null)
+                {
+                    arr[index] = n.value;
+                    index++;
+                    n = n.next;
+                }
+
+                arr[index] = n.value;
+                int[] shortArr = new int[index + 1];
+
+                for (int i = 0; i <= index; i++)
+                    shortArr[i] = arr[i];           
+
+                return shortArr;
+            }
+        }
+
+        public Node get(int index)
+        {
+            if (this.head == null)
+                throw new Exception("The list is empty.");
+            Node n = this.head;
+            for(int i = 0; i < index; i++)
+            {
+                n = n.next;
+                if (n == null)
+                    throw new Exception("Index out of bounds");
+            }
+
+            return n;
+        }
+
+        public Node remove(int index)
+        {
+            Node n = this.head;
+            if (n == null)
+                throw new Exception("The list is empty.");
+            if (index == 0)
+            {               
+                this.head = n.next;
+                return n;
+            }
+            else
+            {
+                Node previous = new Node(0);
+                previous.next = n;
+                for (int i = 0; i < index; i++)
+                {
+                    previous = previous.next;
+                    n = n.next;
+                    if (n == null)
+                        throw new Exception("Index out of bounds");
+                }   
+
+                previous.next = n.next;
+
+                return n;
+            }            
+        }
+
+        public void insert(int index, int val)
+        {
+            Node n = this.head;
+            Node previous = new Node(0);
+            previous.next = n;
+            for (int i = 0; i < index; i++)
+            {
+                previous = previous.next;
+                n = n.next;
+                if (n == null)
+                    throw new Exception("Index out of bounds");
+            }
+
+            Node newNode = new Node(val);
+            previous.next = newNode; 
+            newNode.next = n;                        
+        }
+    }
+}
---
+++ Data Structures/C#/LinkedList/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace myNamespace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList ll = new LinkedList(new int[] { 3, 4, 3, 5, 5 });
+
+            ll.addToStart(99);
+            ll.addToEnd(88);
+            int[] llArray = ll.toArray();
+            foreach (int x in llArray)
+                Console.WriteLine(x);
+
+            try
+            {
+                Console.WriteLine(ll.get(6).value);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+
+            try
+            {
+                ll.remove(2);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+
+            int[] llArray2 = ll.toArray();
+
+            foreach (int x in llArray2)
+                Console.WriteLine(x);
+
+            try
+            {
+                ll.insert(4,666);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+
+            int[] llArray3 = ll.toArray();
+
+            foreach (int x in llArray3)
+                Console.WriteLine(x);
+
+            Console.ReadKey();
+        }
+    }
+}
